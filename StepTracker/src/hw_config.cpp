4061e67849a379d531f5c36cbfa7a9439c319718
Alex-Vella <alex.b.vella@student.uts.edu.au>
2026-06-01 15:25:12 +1000

    hwconfig additions

--- StepTracker/src/hw_config.cpp
+++ StepTracker/src/hw_config.cpp
@@ -1,22 +1,22 @@
 #include "hw_config.h"
 
 void initializeBoardPins(const BoardConfig &config) {
-    pinMode(config.redLedPin, OUTPUT);  
-    pinMode(config.yellowLedPin, OUTPUT);
-    pinMode(config.greenLedPin, OUTPUT);
+  pinMode(config.redLedPin, OUTPUT);  
+  pinMode(config.yellowLedPin, OUTPUT);
+  pinMode(config.greenLedPin, OUTPUT);
   pinMode(config.stPin, OUTPUT);
   
   pinMode(config.xAdcPin, INPUT);
   pinMode(config.yAdcPin, INPUT);
   pinMode(config.zAdcPin, INPUT);
 
-    pinMode(config.pushButtonAPin, INPUT); //has a hardware pulldown externally
-    pinMode(config.pushButtonBPin, INPUT); //has a hardware pulldown externally
+  pinMode(config.pushButtonAPin, INPUT); //has a hardware pulldown externally
+  pinMode(config.pushButtonBPin, INPUT); //has a hardware pulldown externally
 
   // I2C pins are configured when Wire.begin(SDA, SCL) is called.
 
-    digitalWrite(config.redLedPin, LOW);
-    digitalWrite(config.yellowLedPin, LOW);
-    digitalWrite(config.greenLedPin, LOW);
-    digitalWrite(config.stPin, LOW);
+  digitalWrite(config.redLedPin, LOW);
+  digitalWrite(config.yellowLedPin, LOW);
+  digitalWrite(config.greenLedPin, LOW);
+  digitalWrite(config.stPin, LOW);
 }
